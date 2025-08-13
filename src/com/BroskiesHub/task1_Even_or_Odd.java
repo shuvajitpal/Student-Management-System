0578b3968cc14de235269ae5beccdba982f31ee6
shuvajitpal <62866829+shuvajitpal@users.noreply.github.com>
2025-08-13 10:26:45 +0530

    Initial commit

---
+++ src/com/BroskiesHub/task1_Even_or_Odd.java
@@ -0,0 +1,17 @@
+package com.BroskiesHub;
+
+import java.util.Scanner;
+
+public class task1_Even_or_Odd {
+      public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         System.out.print("Enter a number: ");
+         int num = sc.nextInt();
+         if (num % 2 == 0) {
+            System.out.println(num + " is an even number.");
+         } else {
+            System.out.println(num + " is an odd number.");
+         }
+         sc.close();
+      }
+}
